4db66ca2a81606f7d38af825bdd45cc7a1f8f90b
AndreyOlorin <sap7andy@gmail.com>
2025-04-16 19:22:00 +0700

    Настройка

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-kapt")
 }
-
 android {
-    namespace = "com.example.wewatch"
+    namespace = "com.example.lbwatch"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.wewatch"
+        applicationId = "com.example.lbwatch"
         minSdk = 28
         targetSdk = 34
         versionCode = 1
@@ -33,15 +33,45 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
+
+    val moshi_version = "1.15.0"
+    val room_version = "2.6.1"
+
+
+    implementation(libs.androidx.recyclerview)
+
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    kapt("androidx.room:room-compiler:$room_version")
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation (libs.moshi.kotlin)
+    implementation(libs.moshi)
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:$moshi_version")
+
+    implementation (libs.converter.moshi)
+
+    implementation(libs.picasso)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
