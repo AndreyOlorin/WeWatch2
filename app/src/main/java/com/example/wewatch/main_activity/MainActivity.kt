352ecdbe4ef224bd2bc85a67e32c16014b890317
AndreyOlorin <sap7andy@gmail.com>
2025-04-26 19:46:34 +0700

    Настройка БД и модели данных

--- app/src/main/java/com/example/wewatch/main_activity/MainActivity.kt
+++ app/src/main/java/com/example/wewatch/main_activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.wewatch
+package com.example.wewatch.main_activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.wewatch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
